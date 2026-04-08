3d1aabe35dd2db4be62f13f1442c6fd3be1baa02
CesarNML <cesarnml@outlook.com>
2026-04-08 14:41:56 +0700

    fix: Dockerfile repo-root build context, defer api.ts URL validation to call time [P10.01]

--- web/src/lib/server/api.ts
+++ web/src/lib/server/api.ts
@@ -1,12 +1,10 @@
 import { env } from '$env/dynamic/private';
 
-const baseUrl = (env.PIRATE_CLAW_API_URL ?? '').replace(/\/$/, '');
-
-if (!baseUrl) {
-	throw new Error('PIRATE_CLAW_API_URL is required but not set');
-}
-
 export async function apiFetch<T>(path: string): Promise<T> {
+	const baseUrl = (env.PIRATE_CLAW_API_URL ?? '').replace(/\/$/, '');
+	if (!baseUrl) {
+		throw new Error('PIRATE_CLAW_API_URL is required but not set');
+	}
 	const url = `${baseUrl}${path}`;
 	const res = await fetch(url);
 	if (!res.ok) {
